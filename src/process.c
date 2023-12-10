604877a45bc46a9430e72cb064d0a0e807961131
matheusinit <matheus.oliveira.s@protonmail.com>
2023-12-10 17:44:20 -0300

    refactor: put top-level functions at top of file instead of low level ones

--- src/process.c
+++ src/process.c
@@ -9,18 +9,9 @@
 #include <string.h>
 #include <unistd.h>
 
-char *get_path_for_process(char *process_id) {
-  char *processes_folder = "/proc/";
-
-  size_t pid_path_length = strlen(processes_folder) + strlen(process_id);
-
-  char *pid_path = malloc(pid_path_length + 1);
-
-  strcpy(pid_path, processes_folder);
-  strcat(pid_path, process_id);
-
-  return pid_path;
-}
+char **separate_file_by_lines(char *file);
+int get_clock_ticks_by_pid(char *pid);
+int get_total_clock_ticks();
 
 char **get_processes_ids(DIR *directory, int *array_size) {
   struct dirent *files;
@@ -51,60 +42,6 @@ char **get_processes_ids(DIR *directory, int *array_size) {
   }
 
   return array;
-}
-
-int get_clock_ticks_by_pid(char *pid) {
-  char *pid_path = get_path_for_process(pid);
-  char *file = get_file_content("/stat", pid_path);
-  int size = 0;
-
-  char **array = split_string_by_delimiter(file, &size, " ", 52);
-
-  int user_time = atoi(array[13]);
-  int system_time = atoi(array[14]);
-
-  int total_time = user_time + system_time;
-
-  return total_time;
-}
-
-char **separate_file_by_lines(char *file) {
-  int capacity2 = 1024 * 8;
-  int size = 0;
-  char **list = calloc(capacity2 + 1, sizeof(char *));
-
-  char *token = strtok(file, "\n");
-
-  while (token != NULL) {
-    int token_length = strlen(token);
-    list[size] = calloc(token_length + 1, sizeof(char));
-    strcpy(list[size], token);
-    size++;
-
-    token = strtok(NULL, "\n");
-  }
-
-  return list;
-}
-
-int get_total_clock_ticks() {
-  char *file = get_file_content("stat", "/proc");
-
-  char **file_separated_by_lines = separate_file_by_lines(file);
-
-  char *cpu_ticks = file_separated_by_lines[0];
-
-  int size = 0;
-
-  char **array = split_string_by_delimiter(cpu_ticks, &size, " ", 52);
-
-  int total_clock_ticks = 0;
-
-  for (int i = 2; i < size - 5; i++) {
-    total_clock_ticks += atoi(array[i]);
-  }
-
-  return total_clock_ticks;
 }
 
 float *calculate_cpu_usage(char **pid_list, int size) {
@@ -142,4 +79,71 @@ float *calculate_cpu_usage(char **pid_list, int size) {
   }
 
   return cpu_usage_list;
+}
+
+int get_clock_ticks_by_pid(char *pid) {
+  char *pid_path = get_path_for_process(pid);
+  char *file = get_file_content("/stat", pid_path);
+  int size = 0;
+
+  char **array = split_string_by_delimiter(file, &size, " ", 52);
+
+  int user_time = atoi(array[13]);
+  int system_time = atoi(array[14]);
+
+  int total_time = user_time + system_time;
+
+  return total_time;
+}
+
+char *get_path_for_process(char *process_id) {
+  char *processes_folder = "/proc/";
+
+  size_t pid_path_length = strlen(processes_folder) + strlen(process_id);
+
+  char *pid_path = malloc(pid_path_length + 1);
+
+  strcpy(pid_path, processes_folder);
+  strcat(pid_path, process_id);
+
+  return pid_path;
+}
+
+int get_total_clock_ticks() {
+  char *file = get_file_content("stat", "/proc");
+
+  char **file_separated_by_lines = separate_file_by_lines(file);
+
+  char *cpu_ticks = file_separated_by_lines[0];
+
+  int size = 0;
+
+  char **array = split_string_by_delimiter(cpu_ticks, &size, " ", 52);
+
+  int total_clock_ticks = 0;
+
+  for (int i = 2; i < size - 5; i++) {
+    total_clock_ticks += atoi(array[i]);
+  }
+
+  return total_clock_ticks;
+}
+
+char **separate_file_by_lines(char *file) {
+  int capacity2 = 1024 * 8;
+  int size = 0;
+  char **list = calloc(capacity2 + 1, sizeof(char *));
+
+  char *token = strtok(file, "\n");
+
+  while (token != NULL) {
+    int token_length = strlen(token);
+    list[size] = calloc(token_length + 1, sizeof(char));
+    strcpy(list[size], token);
+    size++;
+
+    token = strtok(NULL, "\n");
+  }
+
+  return list;
 }
