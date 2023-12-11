d80aedcdb4ce90b91cec1024ae4ff0086aa7dd4a
matheusinit <matheus.oliveira.s@protonmail.com>
2023-12-11 08:56:41 -0300

    refactor: use struct to gather process information

--- src/main.c
+++ src/main.c
@@ -21,13 +21,15 @@ int main() {
   printf("%s\t%s\t%s\t%s\n", "PID", "STATE", "CPU USAGE(%)", "COMMAND");
 
   for (int index = 0; index < processes_id_size; index++) {
+    struct process current_process;
+
+    current_process.cpu_usage = cpu_usage_list[index];
+
     char *selected_pid = processes_id[index];
 
-    char *pid_path = get_path_for_process(selected_pid);
+    current_process.pid_path = get_path_for_process(selected_pid);
 
-    DIR *pid_dir = check_and_open_directory(pid_path);
-
-    char *pid, *state, *command;
+    DIR *pid_dir = check_and_open_directory(current_process.pid_path);
 
     while ((files = readdir(pid_dir))) {
 
@@ -43,31 +45,34 @@ int main() {
 
       if (strcmp(files->d_name, "stat") == 0) {
 
-        char *file_content = get_file_content(files->d_name, pid_path);
+        char *file_content =
+            get_file_content(files->d_name, current_process.pid_path);
 
         int file_content_array_size = 0;
         file_content_array = split_string_by_delimiter(
             file_content, &file_content_array_size, " ", 52);
 
-        pid = file_content_array[0];
-        state = file_content_array[2];
+        current_process.pid = file_content_array[0];
+        current_process.state = file_content_array[2];
       }
 
       if (strcmp(files->d_name, "cmdline") == 0) {
-        char *file_content = get_file_content(files->d_name, pid_path);
+        char *file_content =
+            get_file_content(files->d_name, current_process.pid_path);
 
-        command = file_content;
+        current_process.command = file_content;
 
-        if (strcmp(command, "") == 0) {
-          command = file_content_array[1];
+        if (strcmp(current_process.command, "") == 0) {
+          current_process.command = file_content_array[1];
         }
       }
     }
 
-    free(pid_path);
+    free(current_process.pid_path);
     closedir(pid_dir);
 
-    printf("%s\t%s\t%.2f\t%s\n", pid, state, cpu_usage_list[index], command);
+    printf("%s\t%s\t%.2f\t%s\n", current_process.pid, current_process.state,
+           current_process.cpu_usage, current_process.command);
   }
 
   free(processes_id);
--- src/process.h
+++ src/process.h
@@ -3,6 +3,14 @@
 
 #include <dirent.h>
 
+struct process {
+  char *pid;
+  char *state;
+  char *command;
+  char *pid_path;
+  float cpu_usage;
+};
+
 char **get_processes_ids(DIR *directory, int *array_size);
 char *get_path_for_process(char *process_id);
 float *calculate_cpu_usage(char **pid_list, int size);
